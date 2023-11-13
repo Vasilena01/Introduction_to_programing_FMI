164dac3292c755261f13474897ce06fd250bb8cc
Vasilena Stanoyska <v.stanoyska@gmail.com>
2023-11-13 18:57:08 +0200

    purva zadacha ot praktikuma

---
+++ Seminari/Seminar 6 (09.11) - zadachi/Seminar 6 (09.11) - zadachi.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+/*Zad 1
+void getArrayInput(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		std::cin >> arr[i];
+	}
+}
+bool checkIfPalindrome(const int arr[], int size) {
+	bool isPalindrome = false;
+	int i = 0;
+	while (arr[i] == arr[size - i - 1]) {
+		if (i != size) {
+			i++;
+			continue;
+		}
+		else {
+			isPalindrome = true;
+			break;
+		}
+	}
+	return isPalindrome;
+}
+int main() {
+	constexpr int MAX_SIZE = 5;
+	int arr[MAX_SIZE];
+	getArrayInput(arr, MAX_SIZE);
+	std::cout << checkIfPalindrome(arr, MAX_SIZE);
+}*/
+
+
+
